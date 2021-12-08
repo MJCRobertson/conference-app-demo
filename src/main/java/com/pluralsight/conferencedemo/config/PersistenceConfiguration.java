74f4e31027bc2290e396f510d3c4ba66baeedafa
Mike Robertson <mjcrobertson@outlook.com>
2021-12-08 15:44:21 +0000

    Commented out POJO Persistance Config method for datasource

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,7 +8,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-
+    /*
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
@@ -16,4 +16,5 @@ public class PersistenceConfiguration {
         System.out.println("My custom datasource bean has been initialized and set");
         return builder.build();
     }
+     */
 }
